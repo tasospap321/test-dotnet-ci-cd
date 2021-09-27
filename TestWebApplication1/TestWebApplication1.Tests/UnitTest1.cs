192ac02312af573ab53f4efbd57838f58036c489
tasospap321 <tasospap321@gmail.com>
2021-09-27 16:39:06 +0200

    Add test project

---
+++ TestWebApplication1/TestWebApplication1.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+
+namespace TestWebApplication1.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.True(true);
+        }
+    }
+}
