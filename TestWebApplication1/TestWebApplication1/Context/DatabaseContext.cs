4d885c72fcf331278ff48935f436a8aaad1f4ece
tasospap321 <tasospap321@gmail.com>
2021-10-08 16:33:14 +0200

    updates

---
+++ TestWebApplication1/TestWebApplication1/Context/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestWebApplication1.Models;
+
+namespace TestWebApplication1.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> UsersContext { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Data Source=.;Initial Catalog=TestWebApplicationDb;Integrated Security=True");
+
+    }
+}
