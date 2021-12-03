97d064417bdc7ad59c137942bf229f8caca54308
tasospap321 <tasospap321@gmail.com>
2021-12-03 10:21:07 +0100

    build

---
+++ TestWebApplication1/TestWebApplication1.Tests/TestTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWebApplication1.Tests
+{
+    [TestFixture]
+    public class TestTest
+    {
+        [OneTimeSetUp]
+        public void TestSetup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
